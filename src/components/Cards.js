6b31e67e76311231883767bfb55d490c2edf16fa
TJ Cooper <tjcoop321@gmail.com>
2018-01-20 12:57:52 -0800

    create card div for images

---
+++ src/components/Cards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = props => 
+
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src={props.imageSrc} alt="Card image cap" />
+  </div>
+
+export default Card;
