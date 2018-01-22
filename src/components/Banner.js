9953bdb2cf9c15af23e4082796b3ee4d6a99a8fb
TJ <cooploops@gmail.com>
2018-01-22 01:10:42 -0800

    card works and got function for click

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const styles = {
     backgroundImg:{
-        "background-color":"linear-gradient(rgba(0,0,0,0.3), rgba(255,255,255,0.5))",
-        "background-image": "url('/assets/images/mario.jpeg')",
-        "background-repeat": "no-repeat",
-        "background-size": "cover",
-        "background-position": "center center"
+        "backgroundColor":"linear-gradient(rgba(0,0,0,0.3), rgba(255,255,255,0.5))",
+        "backgroundImage": "url('/assets/images/mario.jpeg')",
+        "backgroundRepeat": "no-repeat",
+        "backgroundSize": "cover",
+        "backgroundPosition": "center center"
     },
     text:{
         color: "black",
-        'text-shadow': "2px 2px 5px white"
+        'textShadow': "2px 2px 5px white"
     }
 };
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const Card = props => 
+const styles = {
+  container: {
+    "width":"18rem",
+    "height":"15rem"
+  },
+  img:{
+    "width":"100%",
+    "height":"100%",
+    "overflow":"hidden"
+  }
+}
 
-  <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src={props.imageSrc} alt="Card image cap" />
+const Card = props =>
+  <div className="row mx-2">
+      {props.portraits.map(champ =>
+        <div className="card mx-auto my-auto" key={champ.name} name={champ.name} style={styles.container}>
+          <img className="card-img-top" 
+          style={styles.img} 
+          name={champ.name} 
+          src={champ.image} 
+          alt={champ.name} 
+          onClick={props.handleOnClick}/>
+        </div>
+      )}
   </div>
 
 export default Card;
