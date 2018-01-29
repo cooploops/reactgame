c6629218f6618d810f70df902249b86e32ad75b8
TJ <cooploops@gmail.com>
2018-01-28 19:24:18 -0800

    got it working on the deployed side

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,32 +1,32 @@
 import React from 'react';
 
-const styles = { 
-    backgroundImg:{
-        "backgroundColor":"linear-gradient(rgba(0,0,0,0.3), rgba(255,255,255,0.5))",
-        "backgroundImage": "url('/assets/images/mario.jpeg')",
-        "backgroundRepeat": "no-repeat",
-        "backgroundSize": "cover",
-        "backgroundPosition": "center center",
-        "marginBottom":"2px"
-    },
-    text:{
-        color: "black",
-        'textShadow': "2px 2px 1px white",
-        fontFamily:"'Eczar', serif"
-    },
-    smalltext:{
-        color:"black",
-        fontFamily:"'Eczar',serif",
-        textShadow: "0.6px 0.6px 1px white"
-    }
-};
+// const styles = { 
+//     backgroundImg:{
+//         "backgroundColor":"linear-gradient(rgba(0,0,0,0.3), rgba(255,255,255,0.5))",
+//         // "backgroundImage": "url('./mario.jpeg')",
+//         "backgroundRepeat": "no-repeat",
+//         "backgroundSize": "cover",
+//         "backgroundPosition": "center center",
+//         "marginBottom":"2px"
+//     },
+//     text:{
+//         color: "black",
+//         'textShadow': "2px 2px 1px white",
+//         fontFamily:"'Eczar', serif"
+//     },
+//     smalltext:{
+//         color:"black",
+//         fontFamily:"'Eczar',serif",
+//         textShadow: "0.6px 0.6px 1px white"
+//     }
+// };
 
 const Banner = () =>
 
-    <div className="jumbotron jumbotron-fluid backgroundImg" style={styles.backgroundImg}>
+    <div className="jumbotron jumbotron-fluid backgroundImg" >
         <div className="container">
-            <h1 className="display-4 text-center" style={styles.text}>Super Smash Memory Game</h1>
-            <h4 className="text-center" style={styles.smalltext}>Click all of the images only once!</h4>
+            <h1 className="display-4 text-center headerText" >Super Smash Memory Game</h1>
+            <h4 className="text-center smallText" >Click all of the images only once!</h4>
         </div>
     </div>
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
-const styles = {
-  container: {
-    "width":"18rem",
-    "height":"15rem",
-    "background":"rgba(0,0,0,0.6)"
-  },
-  img:{
-    "width":"100%",
-    "height":"100%",
-    "overflow":"hidden"
-  },
-  backgroundStage:{
-    "backgroundImage":"url('/assets/images/superSmashStage.jpg')",
-    "backgroundRepeat": "no-repeat",
-    "backgroundSize": "cover",
-    "backgroundPosition": "center center",
-  }
-}
+// const styles = {
+//   container: {
+//     "width":"18rem",
+//     "height":"15rem",
+//     "background":"rgba(0,0,0,0.6)"
+//   },
+//   img:{
+//     "width":"100%",
+//     "height":"100%",
+//     "overflow":"hidden"
+//   },
+//   backgroundStage:{
+//     // "backgroundImage":"url('./superSmashStage.jpg')",
+//     "backgroundRepeat": "no-repeat",
+//     "backgroundSize": "cover",
+//     "backgroundPosition": "center center",
+//   }
+// }
 
 const Card = props =>
-  <div className="row" style={styles.backgroundStage}>
+  <div className="row backgroundStage" >
       {props.portraits.map(champ =>
-        <div className="card m-auto col-4-lg" key={champ.name} name={champ.name} style={styles.container}>
-          <img className="card-img-top p-3" 
-          style={styles.img} 
+        <div className="card m-auto col-4-lg cardContainer" key={champ.name} name={champ.name} >
+          <img className="card-img-top p-3 imgContainer" 
           name={champ.name} 
           src={champ.image} 
           alt={champ.name} 
